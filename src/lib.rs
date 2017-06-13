ebbc05b35eb705da0bb6937e7a409bada8c160eb
David Allen <david.allen@ey.com>
2017-06-13 13:00:10 -0600

    Trying to export bytes.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate metrics as metrics_lib;
 pub extern crate iron as http;
 
 pub extern crate serde;
+pub extern crate serde_bytes;
 pub extern crate serde_json as json;
 pub extern crate rmp as msgpack;
 pub extern crate rmp_serde as msgpack_serde;
